425e471a30dd787ca3ec3bd556d035c2a5961ea0
neha-asthana-sp <neha.asthana@sailpoint.com>
2017-09-12 11:45:36 -0500

    Add files via upload

---
+++ String_follow.java
@@ -0,0 +1,32 @@
+/* Check if string follows order of characters defined by a pattern or not*/
+public class String_follow {
+	static int count = 0;
+	public static void main(String [] args)
+	{
+		
+		String_follow sf = new String_follow();
+		sf.count1();
+		//System.out.println(sf.count1());
+		sf.count11();
+		String s = "neha";
+		
+	}
+	int count1()
+	
+	{
+		count++;
+		System.out.println("count1:"+count);
+		return count;
+		
+	}
+	void count11()
+	
+	{
+		System.out.println("count11:"+count);
+		count1();
+		
+		
+	}
+	}
+
+
